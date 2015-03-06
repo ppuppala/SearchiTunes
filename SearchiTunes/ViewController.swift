f6858e677897b01144454c4f8ee51d6d6f7fb394
ppuppala <praneetpuppala@gmail.com>
2015-03-06 00:36:45 -0500

    Initial Commit

---
+++ SearchiTunes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchiTunes
+//
+//  Created by Praneet Puppala on 3/6/15.
+//  Copyright (c) 2015 Praneet Puppala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
